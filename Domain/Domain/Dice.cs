58636607e211640bebfcca789431bc0b58b5953d
Sokolova Anna <anna.sok.03@mail.ru>
2022-11-21 21:03:47 +0500

    - ненужное присваивание

--- Domain/Domain/Dice.cs
+++ Domain/Domain/Dice.cs
@@ -19,10 +19,8 @@ public class Dice : ValueType<Dice>
         var casts = new List<int>();
         var random = new Random();
         for (int i = 0; i < Quantity; i++)
-        {
-            var value = random.Next(1, (int)Sides + 1);
-            casts.Add(value);
-        }
+            casts.Add(random.Next(1, (int)Sides + 1));
+
         return casts.ToArray();
     }
 }
