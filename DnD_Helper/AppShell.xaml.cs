f67dec7493b8ae384565a762e63da967cb507d49
AndreyKuklinov <Andreydolphin@mail.ru>
2022-11-21 21:55:38 +0500

    Назад в будущее!

--- DnD_Helper/AppShell.xaml.cs
+++ DnD_Helper/AppShell.xaml.cs
@@ -6,4 +6,9 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 	}
+
+    private void BackToMenu_Clicked(object sender, EventArgs e)
+    {
+		App.Current.MainPage = new MenuShell();
+    }
 }
