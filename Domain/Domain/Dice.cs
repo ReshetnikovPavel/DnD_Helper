2c31e9ffefcb496b97ded068bcab1a7f03071bdd
Sokolova Anna <anna.sok.03@mail.ru>
2022-11-21 21:02:34 +0500

    +кидание дайсов + рандомные абилитискоры

--- Domain/Domain/Dice.cs
+++ Domain/Domain/Dice.cs
@@ -7,4 +7,22 @@ public class Dice : ValueType<Dice>
 	public int Quantity { get; }
 	public DiceName Sides { get; }
 	public override string ToString() => $"{Quantity}d{Sides}";
+
+    public Dice(int quantity, DiceName sides)
+    {
+        Quantity = quantity;
+        Sides = sides;
+    }
+
+    public int[] GetRandomValues()
+    {
+        var casts = new List<int>();
+        var random = new Random();
+        for (int i = 0; i < Quantity; i++)
+        {
+            var value = random.Next(1, (int)Sides + 1);
+            casts.Add(value);
+        }
+        return casts.ToArray();
+    }
 }
--- Domain/Domain/DistributorAbilityScore.cs
+++ Domain/Domain/DistributorAbilityScore.cs
@@ -40,4 +40,25 @@ public class DistributorAbilityScore
     public int GetTotalPoints() => TotalPoints;
 
     public void ResetTotalPoints() => TotalPoints = 27;
+
+    public int[] GetRandomAbilityScoresValues()
+    {
+        var values = new List<int>();
+        for (int i = 0; i < 6; i++)
+            values.Add(GetRandomValueAbility());
+
+        return values.ToArray();
+    }
+
+    private int GetRandomValueAbility()
+    {
+        var dice = new Dice(4, DiceName.D6);
+        var values = dice.GetRandomValues().ToList();
+        values.Remove(values.Min());
+        var sum = 0;
+        for (var i = 0; i < values.Count; i++)
+            sum += values[i];
+
+        return sum;
+    }
 }
