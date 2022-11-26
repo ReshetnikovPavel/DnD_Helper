7b54bec70318a16ec8510b11b99fb402aeae9e1e
AndreyKuklinov <Andreydolphin@mail.ru>
2022-11-26 18:54:54 +0500

    Сумел забиндить буквы

--- DnD_Helper/Resources/Controls/AbilityScoreControl.xaml.cs
+++ DnD_Helper/Resources/Controls/AbilityScoreControl.xaml.cs
@@ -4,32 +4,20 @@ namespace DnD_Helper.Resources.Controls;
 
 public partial class AbilityScoreControl : Grid
 {
-	public static readonly BindableProperty AbilityScoreProperty = BindableProperty.Create(
-		nameof(AbilityScore), typeof(AbilityScore), typeof(AbilityScoreControl));
+	public static readonly BindableProperty ValueDisplayProperty = BindableProperty.Create(
+		nameof(ValueDisplay), typeof(string), typeof(AbilityScoreControl),
+		defaultValue: "default");
 
-    public static readonly BindableProperty CounterSourceProperty = BindableProperty.Create(
-        nameof(CounterSource), typeof(ImageSource), typeof(AbilityScoreControl));
-
-    public AbilityScoreControl()
+	public AbilityScoreControl()
 	{
 		InitializeComponent();
+
+		BindingContext = this;
 	}
 
-    public AbilityScore AbilityScore
+	public string ValueDisplay
 	{
-		get => (AbilityScore)GetValue(AbilityScoreProperty);
-		set => SetValue(AbilityScoreProperty, value);
+		get => GetValue(ValueDisplayProperty).ToString();
+		set => SetValue(ValueDisplayProperty, value);
 	}
-
-    public ImageSource CounterSource
-    {
-        get => (ImageSource)GetValue(CounterSourceProperty);
-        set => SetValue(CounterSourceProperty, value);
-    }
-
-    public string ValueDisplay
-		=> AbilityScore.Value.ToString();
-
-	public string ModifierDisplay
-		=> AbilityScore.Modifier.ToString();
 }
