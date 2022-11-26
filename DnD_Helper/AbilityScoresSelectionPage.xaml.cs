e0a2e3ae3412655484c5ec71c86e70f97f32d778
AndreyKuklinov <Andreydolphin@mail.ru>
2022-11-27 00:11:18 +0500

    Восстановил макет AbilityScoreControl

--- DnD_Helper/AbilityScoresSelectionPage.xaml.cs
+++ DnD_Helper/AbilityScoresSelectionPage.xaml.cs
@@ -5,36 +5,12 @@ namespace DnD_Helper;
 
 public partial class AbilityScoresSelectionPage : ContentPage
 {
-    private string testString = "the sweetest victory";
-    private Abilities abilities = new Abilities(0, 0, 0, 0, 0, 0);
+	private Abilities abilities;
 
 	public AbilityScoresSelectionPage()
 	{
 		InitializeComponent();
 
         BindingContext = this;
-    }
-
-    public AbilityScore Strength
-        => abilities.Strength;
-
-    public AbilityScore Intelligence
-		=> abilities.Intelligence;
-
-    public string TestString
-    {
-        get => testString;
-        set
-        {
-            if (testString == value)
-                return;
-            testString = value;
-            OnPropertyChanged();
-        }
-    }
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        TestString = "What?";
     }
 }
--- DnD_Helper/Resources/Controls/AbilityScoreControl.xaml.cs
+++ DnD_Helper/Resources/Controls/AbilityScoreControl.xaml.cs
@@ -4,26 +4,10 @@ namespace DnD_Helper.Resources.Controls;
 
 public partial class AbilityScoreControl : Grid
 {
-	public static readonly BindableProperty ValueDisplayProperty = BindableProperty.Create(
-		nameof(ValueDisplay), typeof(string), typeof(AbilityScoreControl),
-		defaultValue: "default");
+
 
 	public AbilityScoreControl()
 	{
 		InitializeComponent();
-	}
-
-	public string ValueDisplay
-	{
-		get => GetValue(ValueDisplayProperty).ToString();
-		set => SetValue(ValueDisplayProperty, value);
-	}
-
-	protected override void OnPropertyChanged(string propertyName)
-	{
-		base.OnPropertyChanged(propertyName);
-
-		if(propertyName == ValueDisplayProperty.PropertyName)
-			TestLabel.Text = ValueDisplay;
 	}
 }
