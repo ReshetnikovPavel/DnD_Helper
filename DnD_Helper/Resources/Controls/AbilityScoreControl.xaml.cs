1d815787f99ced55646f40e9798ac7080877ea79
AndreyKuklinov <Andreydolphin@mail.ru>
2022-11-27 00:26:29 +0500

    Смог забиндить картинку

--- DnD_Helper/Resources/Controls/AbilityScoreControl.xaml.cs
+++ DnD_Helper/Resources/Controls/AbilityScoreControl.xaml.cs
@@ -1,13 +1,29 @@
 using Domain;
+using System.Runtime.CompilerServices;
 
 namespace DnD_Helper.Resources.Controls;
 
 public partial class AbilityScoreControl : Grid
 {
-
+	public static readonly BindableProperty CounterSourceProperty = BindableProperty.Create(
+		nameof(CounterSource), typeof(ImageSource), typeof(AbilityScoreControl));
 
 	public AbilityScoreControl()
 	{
 		InitializeComponent();
 	}
+
+	public ImageSource CounterSource
+	{
+		get => (ImageSource)GetValue(CounterSourceProperty); 
+		set => SetValue(CounterSourceProperty, value);
+	}
+
+    protected override void OnPropertyChanged(string propertyName)
+    {
+        base.OnPropertyChanged(propertyName);
+
+		if(propertyName == CounterSourceProperty.PropertyName)
+			CounterImage.Source = CounterSource;
+    }
 }
