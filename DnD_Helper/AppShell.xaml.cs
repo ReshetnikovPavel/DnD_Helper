581bbefcc4bffb1ebe0b7a8dbf034fc40ecd2825
AndreyKuklinov <Andreydolphin@mail.ru>
2022-11-27 17:35:44 +0500

    Исправил поведение кнопки назад

--- DnD_Helper/AppShell.xaml.cs
+++ DnD_Helper/AppShell.xaml.cs
@@ -1,14 +1,38 @@
-﻿namespace DnD_Helper;
+﻿using System.Windows.Input;
+
+namespace DnD_Helper;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
+	public ICommand GoBack { get; }
+    public ICommand GoForward { get; }
+
+    public AppShell()
 	{
 		InitializeComponent();
+
+        GoBack = new Command(OnGoBack, CanGoBack);
+        GoForward = new Command(OnGoForward, CanGoForward);
+        
+        BindingContext = this;
 	}
 
     private void BackToMenu_Clicked(object sender, EventArgs e)
     {
 		App.Current.MainPage = new MenuShell();
+    }
+
+    private void OnGoBack() { App.Current.MainPage = new MenuShell(); }
+
+    private bool CanGoBack() { return true; }
+
+    private void OnGoForward() { }
+
+    private bool CanGoForward() { return true; }
+
+    protected override bool OnBackButtonPressed()
+    {
+        GoBack.Execute(null);
+        return true;
     }
 }
