5a9d1922dc6fae922556dc2df160b0a96edef95f
AndreyKuklinov <Andreydolphin@mail.ru>
2022-11-28 23:51:23 +0500

    Написал всю свою IHasRoute жуть

---
+++ DnD_Helper/ApplicationClasses/IHasRoute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD_Helper.ApplicationClasses
+{
+    internal interface IHasRoute
+    {
+        string Route { get; }
+        Func<bool> CheckCondition { get; }
+
+        void Go();
+    }
+}
---
+++ DnD_Helper/ApplicationClasses/RouteItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD_Helper.ApplicationClasses
+{
+    internal class RouteItem : IHasRoute
+    {
+        public string Route { get; }
+        public Func<bool> CheckCondition { get; }
+        
+        public RouteItem(string route, Func<bool> condition)
+        {
+            Route = route;
+            CheckCondition = condition;
+        }
+
+        public RouteItem(string route)
+        {
+            Route = route;
+            CheckCondition = () => true;
+        }
+
+        public async void Go()
+        {
+            if (!CheckCondition())
+                return;
+            await Shell.Current.GoToAsync($"///{Route}");
+        }
+    }
+}
