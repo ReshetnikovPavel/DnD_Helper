57f4ff560a6727223c1efec40bfd4b5f978bed2d
AndreyKuklinov <Andreydolphin@mail.ru>
2022-11-29 08:05:55 +0500

    Наработки

--- DnD_Helper/CharacterSheetPage.xaml.cs
+++ DnD_Helper/CharacterSheetPage.xaml.cs
@@ -1,3 +1,5 @@
+using Domain;
+
 namespace DnD_Helper;
 
 public partial class CharacterSheetPage : ContentPage
@@ -9,5 +11,19 @@ public partial class CharacterSheetPage : ContentPage
 		BindingContext = this;
 	}
 
+	public string NameDisplay
+		=> AppShell.Singleton.Character.Name;
 
+    public string MainInfoDisplay
+	{
+		get
+		{
+			var ch = AppShell.Singleton.Character;
+			var race = ch.Race.SubraceName == null ? ch.Race.Name : ch.Race.SubraceName;
+			return $"{race}, {ch.Class.Name}, {ch.Background}";
+		}
+	}
+
+	public string SpeedDisplay
+		=> AppShell.Singleton.Character.Speed.Value.ToString();
 }
