4b0c2787f7b18f5ebb511a0c38b7f5d0b6ba939a
AndreyKuklinov <Andreydolphin@mail.ru>
2022-12-13 17:23:52 +0500

    Туториальный файл + всемирная кнопочная перекраска

---
+++ DnD_Helper/ViewModels/CharacterSelectionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DnD_Helper.ViewModels
+{
+    class CharacterSelectionModel : BindableObject
+    {
+        public ICommand SelectCharacter { get; }
+        public ICommand JoinParty { get; }
+        public ICommand CreateParty { get; }
+
+        public int[] Arr
+            => new int[] { 0, 1, 2, 3 };
+
+
+    }
+}
