dd4dbcf9e84f18f78e352fd1607ccfaffcc787f7
AndreyKuklinov <Andreydolphin@mail.ru>
2022-12-14 15:20:36 +0500

    Готовлюсь к зловещему сабрейсу

--- DnD_Helper/SubraceSelectionPage.xaml.cs
+++ DnD_Helper/SubraceSelectionPage.xaml.cs
@@ -1,4 +1,5 @@
 using DnD_Helper.ApplicationClasses;
+using DnD_Helper.ViewModels;
 
 namespace DnD_Helper;
 
@@ -7,22 +8,6 @@ public partial class SubraceSelectionPage : ContentPage
 	public SubraceSelectionPage()
 	{
 		InitializeComponent();
-		BindingContext = this;
-
-		UpdateItemSource(this, "");
-		MessagingCenter.Subscribe<RaceSelectionPage, string>(this, MessageTypes.SelectionMade.ToString(), 
-			UpdateItemSource);
+		BindingContext = new SubraceSelectionModel();
 	}
-
-	private void UpdateItemSource(object sender, string raceName)
-	{
-		//SubraceListView.ItemsSource = AppShell.Singleton.GetSubraceNames();
-    }
-
-	private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
-	{
-		//AppShell.Singleton.SelectedSubRaceName = e.Item.ToString();
-  //      MessagingCenter.Send<ContentPage, string>(this, Messages.PageCompleted.ToString(),
-  //          nameof(SubraceSelectionPage));
-    }
 }
