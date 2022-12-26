dc5495b83b3f9ea3694a8736de58b8266935aec4
Sokolova Anna <anna.sok.03@mail.ru>
2022-12-26 18:23:30 +0500

    +Штуки в главную страницу и еще классы поправила

--- DnD_Helper/ViewModels/CharacterModel.cs
+++ DnD_Helper/ViewModels/CharacterModel.cs
@@ -19,21 +19,55 @@ namespace DnD_Helper.ViewModels
         public string Background => "Criminal";
         public int Speed => 30;
         public int ArmourClass => 10;
+        public int Money => 1000;
         public int HitPoints => 10;
         public int Initiative => 3;
         public Abilities Abilities => Abilities.CreateDefault();
         public Skills Skills => Skills.CreateDefault();
         public SavingThrows SavingThrows => SavingThrows.CreateDefault();
-        // public int[] SavingThrows => new int[6]{1, 2, 3, 4, 5, 6};
-        // public int[] Skills => new int[18]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-
-        // private Skill[] GetSkills()
-        //     =>  new Skill[18]{new Skill(SkillName.Acrobatics, new AbilityScore(AbilityName.Dexterity, 8), new ProficiencyBonus(2)), new Skill(SkillName.AnimalHandling, new AbilityScore(AbilityName.Wisdom, 8), new ProficiencyBonus(2)), new Skill(SkillName.Arcana, new AbilityScore(AbilityName.Intelligence, 8), new ProficiencyBonus(2)), new Skill(SkillName.Athletics, new AbilityScore(AbilityName.Strength, 8), new ProficiencyBonus(2)), new Skill(SkillName.Deception, new AbilityScore(AbilityName.Charisma, 8), new ProficiencyBonus(2)), new Skill(SkillName.History, new AbilityScore(AbilityName.Intelligence, 8), new ProficiencyBonus(2)), new Skill(SkillName.Insight, new AbilityScore(AbilityName.Wisdom, 8), new ProficiencyBonus(2)), new Skill(SkillName.Intimidation, new AbilityScore(AbilityName.Charisma, 8), new ProficiencyBonus(2)), new Skill(SkillName.Investigation, new AbilityScore(AbilityName.Intelligence, 8), new ProficiencyBonus(2)), new Skill(SkillName.Medicine, new AbilityScore(AbilityName.Wisdom, 8), new ProficiencyBonus(2)), new Skill(SkillName.Nature, new AbilityScore(AbilityName.Intelligence, 8), new ProficiencyBonus(2)), new Skill(SkillName.Perception, new AbilityScore(AbilityName.Wisdom, 8), new ProficiencyBonus(2)), new Skill(SkillName.Performance, new AbilityScore(AbilityName.Charisma, 8), new ProficiencyBonus(2)), new Skill(SkillName.Persuasion, new AbilityScore(AbilityName.Charisma, 8), new ProficiencyBonus(2)), new Skill(SkillName.Religion, new AbilityScore(AbilityName.Intelligence, 8), new ProficiencyBonus(2)), new Skill(SkillName.SleightOfHand, new AbilityScore(AbilityName.Dexterity, 8), new ProficiencyBonus(2)), new Skill(SkillName.Stealth, new AbilityScore(AbilityName.Dexterity, 8), new ProficiencyBonus(2)), new Skill(SkillName.Survival, new AbilityScore(AbilityName.Wisdom, 8), new ProficiencyBonus(2))};
-
-        // private SavingThrow[] GetSavingThrows()
-        //     => new SavingThrow[6]{new SavingThrow(new AbilityScore(AbilityName.Strength, 8), new ProficiencyBonus(2)), new SavingThrow(new AbilityScore(AbilityName.Dexterity, 8), new ProficiencyBonus(2)), new SavingThrow(new AbilityScore(AbilityName.Constitution, 8), new ProficiencyBonus(2)), new SavingThrow(new AbilityScore(AbilityName.Intelligence, 8), new ProficiencyBonus(2)), new SavingThrow(new AbilityScore(AbilityName.Wisdom, 8), new ProficiencyBonus(2)), new SavingThrow(new AbilityScore(AbilityName.Charisma, 8), new ProficiencyBonus(2))};
+        public string Armor => "Leather";
+        public List<Weapon> WeaponsProficiencies => new List<Weapon> { new Weapon("Dagger"), new Weapon("Word"), new Weapon("Sword") };
+        public List<Instrument> InstrumentProficiencies => new List<Instrument> { new Instrument("палкакопалка"), new Instrument("Паша"), new Instrument("ручка") };
+        public List<Armor> ArmorProficiencies => new List<Armor> { new Armor("Кождоспех"), new Armor("металлдоспех"), new Armor("словесный доспех") };
+        public List<Weapon> Weapons => new List<Weapon> { new Weapon("Dagger", new Dice(1, DiceName.D4), "кол."), new Weapon("Word", new Dice(1, DiceName.D20), "кол."), new Weapon("Sword", new Dice(1, DiceName.D6), "кол.") };
+        public List<Equipment> Equipments => new List<Equipment> { new Equipment("Dagg333er"), new Equipment("Wor333d"), new Equipment("Swor333d") };
+        public ProficiencyBonus ProficiencyBonus => new ProficiencyBonus(2);
+        public string Languages => "Common, Elvish";
     }
 
+    public class Equipments : ValueType<Equipments>, IDndObject
+    {
+        public Equipments(Equipment equipment1, Equipment equipment2)
+        {
+            Equipment1 = equipment1;
+            Equipment2 = equipment2;
+        }
+
+        public Equipment Equipment1 { get; }
+        public Equipment Equipment2 { get; }
+
+        public static Equipments CreateDefault()
+        {
+            return new Equipments(new Equipment("Dagger"), new Equipment("Word"));
+        }
+    }
+
+    public class Weapons : ValueType<Weapons>, IDndObject
+    {
+        public Weapons(Weapon weapon1, Weapon weapon2)
+        {
+            Weapon1 = weapon1;
+            Weapon2 = weapon2;
+        }
+
+        public Weapon Weapon1 { get; }
+        public Weapon Weapon2 { get; }
+
+        public static Weapons CreateDefault()
+        {
+            return new Weapons(new Weapon("Dagger", new Dice(1, DiceName.D4), "кол."), new Weapon("Word", new Dice(1, DiceName.D20), "кол."));
+        }
+    }
     public class Skills : ValueType<Skills>, IDndObject
     {
         public Skill Acrobatics { get; }
@@ -109,4 +143,3 @@ namespace DnD_Helper.ViewModels
         }
     }
 }
-
