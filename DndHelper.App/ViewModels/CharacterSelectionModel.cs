06f465306d77980546e899b0aa84cbbbb44c4d07
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2023-01-05 01:02:29 +0500

    Переход из CharacterSelectionPage в менб создания персонажа

--- DndHelper.App/ViewModels/CharacterSelectionModel.cs
+++ DndHelper.App/ViewModels/CharacterSelectionModel.cs
@@ -1,15 +1,28 @@
 ﻿using System.Windows.Input;
+using DndHelper.App.RouteNavigation;
+using DndHelper.Domain.Repositories;
 
 namespace DndHelper.App.ViewModels
 {
     public class CharacterSelectionModel : BindableObject
     {
+        private readonly ICharacterRepository characterRepository;
+        private readonly IShellNavigator shellNavigator;
         public ICommand SelectCharacter { get; }
-        public ICommand CreateNewCharacter { get; }
+        public ICommand CreateNewCharacter => new Command(OnCreateNewCharacter);
 
         public string[] CharacterNames
             => new string[] { "Люля", "Пельмешек", "Поль Реш", "Синий", "Симонов" };
 
+        public CharacterSelectionModel(ICharacterRepository characterRepository, IShellNavigator shellNavigator)
+        {
+            this.characterRepository = characterRepository;
+            this.shellNavigator = shellNavigator;
+        }
 
+        private void OnCreateNewCharacter()
+        {
+            shellNavigator.GoToCharacterCreation();
+        }
     }
 }
