baf1f5f8dc0a606341a5074ab04ad2d8293b8d17
Sokolova Anna <anna.sok.03@mail.ru>
2023-01-19 02:50:03 +0500

    + персонаж для присоединения к партии

--- DndHelper.App/ViewModels/JoinNewPartyModel.cs
+++ DndHelper.App/ViewModels/JoinNewPartyModel.cs
@@ -1,6 +1,11 @@
 using DndHelper.App.Authentication;
 using System.ComponentModel;
 using DndHelper.Infrastructure;
+using System.Net;
+using System.Windows.Input;
+using DndHelper.App.RouteNavigation;
+using DndHelper.Domain.Dnd;
+using DndHelper.Domain.Repositories;
 
 namespace DndHelper.App.ViewModels
 {
@@ -8,6 +13,9 @@ namespace DndHelper.App.ViewModels
     {
         private IAuthenticationProvider<string> authProvider;
         private string id;
+        public string[] characters => new string[3] { "AA", "aaaa", "AAAAAAAAAA" };
+        public ICommand SelectCharacter { get; }
+        
 
         public event PropertyChangedEventHandler PropertyChanged;
 
