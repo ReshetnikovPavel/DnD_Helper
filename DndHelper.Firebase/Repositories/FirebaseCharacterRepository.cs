836455e237d1d82c19c56665f81ee8680993e3d9
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2023-01-31 11:56:46 +0500

    Убрал бред, написанный ночью, и теперь персонажи загружаются быстрее

--- DndHelper.Firebase/Repositories/FirebaseCharacterRepository.cs
+++ DndHelper.Firebase/Repositories/FirebaseCharacterRepository.cs
@@ -41,23 +41,13 @@ public class FirebaseCharacterRepository : ICharacterRepository<HttpStatusCode>
 
     public async Task<Result<IEnumerable<Character>, HttpStatusCode>> GetCharacters()
     {
-        var characters = new List<Character>();
         var res = await HandleError(async () =>
-            (await GetUserQuery().Child("Characters").OnceSingleAsync<Dictionary<string,Character>>()));
-        if (res.TryGetValue(out var dictionary))
-        {
-            var ids = dictionary.Keys;
-            foreach (var id in ids)
-            {
-                var characterResult = await GetCharacter(Guid.Parse(id));
+            await GetUserQuery()
+                .Child("Characters")
+                .OnceSingleAsync<Dictionary<string,Character>>());
 
-                if (characterResult.TryGetValue(out var character))
-                    characters.Add(character);
-            }
-
-            if (characters.Any())
-                return Result.CreateSuccess<IEnumerable<Character>, HttpStatusCode>(characters);
-        }
+        if (res.TryGetValue(out var idToCharacter))
+            return idToCharacter.Values;
         return Result.CreateFailure<IEnumerable<Character>, HttpStatusCode>(res.Status);
     }
 
